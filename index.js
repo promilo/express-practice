3b20c5f7b15d4c96ea1c4e403420515b2794d1db
promilo <milind.shah@mail.utoronto.ca>
2017-09-10 15:13:14 -0400

    mini params express

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.send('hello')
 } );
 
+app.get('/names/:name', (req, res) => {
+  const name = req.params.name
+  res.send(`Hello ${name}`)
+  
+})
+
+
+
 app.listen(3000, () => {
   console.log('asdfasdf');
 })
